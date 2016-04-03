8a53db202a583fa64cb77ff0364f1186fc5dc267
vagrant <vagrant@precise32.(none)>
2016-04-03 17:36:14 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codeblue::Application.config.secret_key_base = '25de1863b870d2f10d616dab2905e2a6489469b22218a030b83dab2e34e9b8451b733075778fdc626fcd82a18039f4aef9dd6546626b7dd8c3764912e9a22e69'
