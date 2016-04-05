5ef396539370bf5a71707115f206b79524e291c4
vagrant <vagrant@precise32.(none)>
2016-04-05 02:52:56 +0000

    Installed devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Codeblue::Application.configure do
+  config.action_mailer.default_url_options = { host: 'codeblue-maurae.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
