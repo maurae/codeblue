5d653f6d3dfff216ed8c5518130f1b95c01b2ede
vagrant <vagrant@precise32.(none)>
2016-04-06 21:46:56 +0000

    create action and show action add view for front end

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -5,4 +5,17 @@ class Instructor::CoursesController < ApplicationController
 		@course = Course.new
 	end
 
+	def create
+		@course = current_user.courses.create(course_params)
+		redirect_to instructor_course_path(@course)
+	end
+
+	def course_params
+		params.require(:course).permit(:title, :description, :cost) 
+	end
+
+	def show
+		@course = Course.find(params[:id])
+	end
+
 end
