0a5889c60aab7f4596e6e92b123382ecfb49cb51
vagrant <vagrant@precise32.(none)>
2016-04-15 18:39:24 +0000

    undefined method 'mount_uploader;

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-  mount_uploader :image, CourseUploader
+  mount_uploader :image, ImageUploader
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
